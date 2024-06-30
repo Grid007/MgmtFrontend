e403d7bffd8606dc824a7aa4aa59e9c9fb4614a2
Aniketh Shetty <111851589+Grid007@users.noreply.github.com>
2024-06-30 13:17:58 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ const cache = new InMemoryCache({
 });
 
 const client = new ApolloClient({
-  uri: 'https://workflowutility.onrender.com/graphql',
+  uri: 'https://{your-backend-url}/graphql',
   cache,
 });
 
